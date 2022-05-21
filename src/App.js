f4835bc5a9e8d91983224a272e3f8ba9ae6b552a
opppdia <hannan.opppdia@gmail.com>
2022-05-21 09:35:17 +0600

    re pushed on git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Practice from "./pages/Home/Practice";
+
+function App() {
+  return (
+    <>
+      <Home />
+      {/* <Practice /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Practice.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Practice() {
+  return (
+    <div className="practice">
+      Practice
+      <iframe
+        src="https://player.vimeo.com/video/708244501?h=a599b9c8aa"
+        width="640"
+        height="337"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default Practice;
